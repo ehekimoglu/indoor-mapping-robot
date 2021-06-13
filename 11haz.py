b9b13a942e703e1f0b839dfbe62a9627933f0b5f
iremsukopan <65950841+iremsukopan@users.noreply.github.com>
2021-06-13 19:52:16 +0300

    Update 11haz.py

--- 11haz.py
+++ 11haz.py
@@ -300,6 +300,29 @@ while counter == 0: #if completion hasnt been reached
         if 30 < time.time() - next_time: # after robot cruises for 30 secs
            counter =  completion(counter, wall_length, turn_list, turn_angle) #draw map
         continue  
-        
+
+
+#READ THE JPG FILE AND ENCODE IT
+fileName ='map.jpg'
+
+#encode
+with open(fileName, "rb") as image_file:
+    encoded_string = base64.b64encode(image_file.read())
+
+class RequestHandler_httpd(BaseHTTPRequestHandler):
+    def do_GET(self):
+        message_to_send = encoded_string
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain')
+        self.send_header('Content-Length', len(message_to_send))
+        self.end_headers()
+        self.wfile.write(message_to_send)
+        return
+
+#create server connection and send encoded string
+server_address_httpd = ('192.168.68.118', 8080)
+httpd = HTTPServer(server_address_httpd, RequestHandler_httpd)
+print('Starting server: ')
+httpd.serve_forever()        
             
         
