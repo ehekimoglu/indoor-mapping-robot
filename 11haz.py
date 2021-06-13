3d6daba0d8cba8ef6eaa87473ce7293a374c8ad1
ehekimoglu <82401047+ehekimoglu@users.noreply.github.com>
2021-06-13 19:25:42 +0300

    Add files via upload

--- 11haz.py
+++ 11haz.py
@@ -5,6 +5,9 @@ import RPi.GPIO as GPIO
 import turtle
 import smbus
 import math
+from PIL import Image
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import base64
 
 #pins are in BCM
 GPIO.setmode(GPIO.BCM)
@@ -16,8 +19,10 @@ r_trig = 23
 r_echo = 24
 
 check = 0
-curve = 0.0
-
+curve = 0
+bump_in_timer = 0
+bump_out_timer = 0
+front_wall_timer = 0
 # ACCELEROMETER
 address = 0x53
 bus = smbus.SMBus(1)
@@ -25,7 +30,7 @@ bus.write_byte_data(address, 0x2C, 0x0A)
 bus.write_byte_data(address, 0x2D, 0x08)
 bus.write_byte_data(address, 0x31, 0x08)
 
-t = turtle.Turtle()
+state = 0
 
 # FIND A WALL
 f_treshold = 35
@@ -57,7 +62,7 @@ coordinate = [0,0]
 left = 1
 right = -1
 distance = 0
-v_init = 78
+v_init = 22
 end_time = time.time()
 change = 0
 
@@ -67,7 +72,7 @@ away_time = 0
 closer_time = 0
 
 # GET DISTANCE DATA
-def get_dist(myTrig, myEcho):  # (23,24) ve diğeri
+def get_dist(myTrig, myEcho):  # getting data from distance sensor 
 
     TRIG = myTrig
     ECHO = myEcho
@@ -91,10 +96,10 @@ def get_dist(myTrig, myEcho):  # (23,24) ve diğeri
 
     distance = round(distance, 2)
 
-    if TRIG == f_trig:
-        print("Front distance:", distance, "cm")
-    elif TRIG == r_trig:
-        print("Right distance:", distance, "cm")
+#     if TRIG == f_trig:
+#         print("Front distance:", distance, "cm")
+#    if TRIG == r_trig:
+#         print("Right distance:", distance, "cm")
 
     return distance
 
@@ -110,27 +115,42 @@ def get_accel(address):
 #    print("Acceleration in Y-Axis : ", -yAccl)
     return -yAccl
 
-def completion(counter, wall_length, turn_list, turn_angle):
+def completion(counter, wall_length, turn_list, turn_angle): #map creation algorithm
     robot.stop()
+    global turtle
+    global t
     t = turtle.Turtle()
-    print(len(wall_length))
-    print(len(turn_list))
-    for i in range(len(wall_length)):
-        print("calculating wall" ,i)
+#    print(len(wall_length))
+#    print(len(turn_list))
+    for i in range(len(wall_length)): #creating map
+        print("calculating wall " ,i+1 ,", length  ", int(wall_length[i]), " cm")
         t.forward(wall_length[i]*scale)
-#        if turn_list[i] == left:
-        t.left(turn_angle)
-#        elif turn_list[i] == right:
-#            t.right(turn_angle)
+        if turn_list[i] == left:
+            t.left(turn_angle)
+        elif turn_list[i] == right:
+            t.right(turn_angle)
     print("Done.")
     sc = turtle.getscreen()
-    sc.getcanvas().postscript(file="duck.eps")
+    sc.getcanvas().postscript(file="map.eps")
 #    turtle.done()
+    fileName = 'map.eps'
+    screen = turtle.Screen()
+    screen.setup(1000, 1000)
+    turtle = turtle.Turtle(visible=False)
+    screen.tracer(False)
+    screen.tracer(True)
+    canvas = screen.getcanvas()
+    canvas.postscript(file=fileName)
+    canvas.postscript(file=fileName, width=1000, height=1000)
+
+#converting .eps to .jpg
+    img = Image.open(fileName)
+    img.save("map.jpg", "JPEG")
     print("Saved")
     counter += 1
     return counter
 
-def findWalls(r_trig, r_echo, f_trig, f_echo):
+def findWalls(r_trig, r_echo, f_trig, f_echo): #how the robot initialy finds walls
     right_wall_detected = True
 
     if not get_dist(r_trig, r_echo) <= r_treshold:
@@ -139,17 +159,17 @@ def findWalls(r_trig, r_echo, f_trig, f_echo):
 #        robot.left(0.7)
 #        time.sleep(0.2)
     while  get_dist(f_trig,f_echo)<=400 and 30 <= get_dist(f_trig,f_echo) and not right_wall_detected:
-        print("Getting closer to front wall")
-        robot.forward(0.7, curve_right = curve)
+#        print("Getting closer to front wall")
+        robot.forward(0.7)
         time.sleep(0.2)
     while not right_wall_detected:
-        print("looking for wall...")
+#        print("looking for wall...")
         right_wall_detected = where_is_my_wall(r_trig, r_echo, f_trig, f_echo)        
     return right_wall_detected
 
-def where_is_my_wall(r_trig, r_echo,f_trig,f_echo):
+def where_is_my_wall(r_trig, r_echo,f_trig,f_echo): #wall finding 360 loop
     robot.left(0.4)
-    print("Searching for wall...")
+#    print("Searching for wall...")
     time.sleep(0.1)
     robot.stop()
     right_wall_detected = False
@@ -160,7 +180,7 @@ def where_is_my_wall(r_trig, r_echo,f_trig,f_echo):
         robot.stop()
     return right_wall_detected
 
-def move_closer():
+def move_closer(): #keep treshold distance
     robot.right(0.4)
     time.sleep(0.1)
 #    robot.left(0.3)
@@ -168,7 +188,7 @@ def move_closer():
 #    robot.stop
     return
 
-def move_away():
+def move_away(): #keep treshold distance
     robot.right(0.4)
     time.sleep(0.1)
 #    robot.right(0.3)
@@ -176,97 +196,109 @@ def move_away():
 #    robot.stop
     return
 
-while counter == 0:
-    right_wall_detected = findWalls(r_trig, r_echo, f_trig, f_echo)
-    previousRight = get_dist(r_trig, r_echo)
-    next_time = time.time()
-    while right_wall_detected and counter == 0:
+while counter == 0: #if completion hasnt been reached
+    right_wall_detected = findWalls(r_trig, r_echo, f_trig, f_echo) #find right wall
+    previousRight = get_dist(r_trig, r_echo) #for keeping the wall at a fixed distance and bump in/out handling
+    next_time = time.time() #for completion
+    while right_wall_detected and counter == 0: 
         time.sleep(0.1)
         RightDistance = get_dist(r_trig, r_echo)
-        if RightDistance > 200:
+        if RightDistance > 100: # finding right wall by turning right
             right_wall_detected = False
             print("Right wall lost...")
             temp = 0
-        while not right_wall_detected:
-            robot.right(0.7)
+        while not right_wall_detected: 
+            robot.right(0.5)
             print("searching for LOST wall")
             time.sleep(0.1)
             robot.stop()
             if temp ==3:
-                robot.forward(0.7, curve_right = curve)
+                robot.forward(0.7)
                 time.sleep(0.5)
                 robot.stop()
             temp +=1
             RightDistance = get_dist(r_trig, r_echo)
             if RightDistance <= r_treshold:
                 right_wall_detected = True
-        if RightDistance <= 10 and 3 <= end_time - away_time:
+        if RightDistance <= 10 and 3 <= end_time - away_time: #robot too close to the wall
             move_away()
             away_time = time.time()
-        if 40 <=RightDistance and 2.8 <= end_time - closer_time:
+        if 40 <=RightDistance and 3 <= end_time - closer_time: #robot to far from the wall
             move_closer()
             closer_time = time.time()
-        robot.forward(0.7, curve_right = curve)
-        print("Cruising...")
-        time.sleep(0.15)
+        robot.forward(0.7, curve_left = curve)
+        if state == 0: #only once a wall
+            print("Cruising...")
+            state = 1
+#        time.sleep(0.15)
         start_time = end_time
         end_time = time.time()
-        elapsed_time = end_time - (start_time + 0.5)
-        accel = get_accel(address)/100
-        distance += elapsed_time *(accel * elapsed_time / 2 + v_init)
-        v_init += elapsed_time * accel
+        elapsed_time = end_time - (start_time)
+        accel = get_accel(address)/100 #conversion
+        distance += elapsed_time *(accel * elapsed_time / 2 + v_init) #calculate wall length
+        v_init += elapsed_time * accel #update velocity
         
         FrontDistance = get_dist(f_trig, f_echo)
-        if FrontDistance <= f_treshold:
+        if FrontDistance <= f_treshold: #front wall detected
             front_wall_detected = True
             robot.stop()
             print("!!FRONT WALL DETECTED!!")
-#            next_wall_distance = get_dist(r_trig, f_trig)
+            next_wall_distance = get_dist(r_trig, r_echo)
             robot.stop()
             right_wall_detected = False
-            robot.left(1)
-            time.sleep(0.25)
+            robot.left(0.9)
+            time.sleep(0.2)
             robot.stop()
             while not right_wall_detected:
                 right_wall_detected = where_is_my_wall(r_trig, r_echo,f_trig,f_echo)
-            robot.right(0.4)
-            time.sleep(0.15)
-            wall_length.append(distance)
+#            robot.right(0.2)
+#            time.sleep(0.05)
+            wall_length.append(distance+FrontDistance)
             orientation = orientation % 4
             orientation_list.append(orientation)
             turn_list.append(left)
             orientation += left
-#            distance = next_wall_distance
-            v_init = 78
+            distance = next_wall_distance
+            front_wall_timer = 0
+            v_init = 22
+            state = 0
             
-#        if 10 <= abs(RightDistance - previousRight):
-#            change = RightDistance - previousRight
+        if 20 <= abs(RightDistance - previousRight) and abs(RightDistance- previousRight) <= 40: #is it a bump?
+            change = RightDistance - previousRight
         previousRight = RightDistance
         # continue cruising
             # If there is a small bump on the route
-#        if r_treshold > change > 10:
-#            new_wall = change
-#            orientation += right
-#            orientation = orientation % 4
-#            orientation_list.append(orientation)
-#            turn_list.append(right)
-#            wall_length.append(new_wall * scale)
-#            orientation += left
-#            turn_list.append(left)
-#            distance = 0
+        if r_treshold > change > 10 and 3 < time.time()- bump_out_timer and 3 < time.time()-front_wall_timer: #bump handling
+            time.sleep(0.5)
+            check = get_dist(r_trig, r_echo)
+            if abs(check- RightDistance) < 5:
+                new_wall = change
+                orientation += right
+                orientation = orientation % 4
+                orientation_list.append(orientation)
+                turn_list.append(right)
+                wall_length.append(new_wall * scale)
+                orientation += left
+                turn_list.append(left)
+                distance = 0
+                bump_out_timer = time.time()
 
-#        elif -30 < change < -10:
-#            new_wall = -change
-#            orientation += left
-#            orientation = orientation % 4
-#            orientation_list.append(orientation)
-#            turn_list.append(left)
-#            wall_length.append(new_wall * scale)
-#            orientation += right
-#            turn_list.append(right)
-#            distance = 0
-        if 30 < time.time() - next_time: 
-           counter =  completion(counter, wall_length, turn_list, turn_angle)
+        elif -30 < change < -10 and 3 < time.time()- bump_in_timer and 3 < time.time()-front_wall_timer: #bump handling
+            time.sleep(0.5)
+            check = get_dist(r_trig, r_echo)
+            if abs(check - RightDistance) < 5:
+                new_wall = -change
+                orientation += left
+                orientation = orientation % 4
+                orientation_list.append(orientation)
+                turn_list.append(left)
+                wall_length.append(new_wall * scale)
+                orientation += right
+                turn_list.append(right)
+                distance = 0
+                bump_in_timer = time.time()
+        if 30 < time.time() - next_time: # after robot cruises for 30 secs
+           counter =  completion(counter, wall_length, turn_list, turn_angle) #draw map
         continue  
         
             
